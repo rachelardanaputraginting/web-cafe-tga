5a8222fde3e0b99312bf022fb0fcea179712ab8c
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-05-17 16:38:00 +0700

    Memperbaiki tampilan halaman rincian product

---
+++ database/migrations/2023_05_17_092328_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('cart_id');
+            $table->double('gross_amount');
+            $table->string('status')->default(0);
+            $table->json('card_ids');
+            $table->dateTime('succeeded_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
--- resources/js/Pages/Products/Show.jsx
+++ resources/js/Pages/Products/Show.jsx
@@ -10,19 +10,19 @@ export default function Show({ product }) {
         <>
             <Head title="Show" />
             <Container>
-                <div className="flex gap-10 w-full">
+                <div className="flex gap-10 w-full mt-44">
                     <div className="w-1/3">
-                        <img className='w-full' src={product.picture} alt="" />
+                        <img className='w-full rounded-lg' src={product.picture} alt="" />
                     </div>
-                    <div className="w-2/3 min-h-full flex flex-col justify-between">
-                        <div className="flex-1">
-                            <Link className='text-xs font-semibold px-2 py-1 inline-flex bg-blue-500 text-white rounded' href={`/products?category=${product.category.slug}`}>{product.category.name}</Link>
-                            <h1 className='text-3xl font-semibold'>{product.name}</h1>
-                            <div className='leading-relaxed text-gray-500 my-4'>{product.description}</div>
-                            <div className='font-semibold text-4xl'><sup>Rp</sup> {numberFormat(product.price)}</div>
+                    <div className="w-2/3 min-h-auto flex flex-col justify-start">
+                        <div>
+                            <Link className='text-xs font-semibold px-2 py-1 w-auto bg-blue-500 text-white rounded' href={`/products?category=${product.category.slug}`}>{product.category.name}</Link>
                         </div>
+                        <h1 className='text-3xl font-semibold'>{product.name}</h1>
+                        <div className='leading-relaxed text-gray-500 my-4'>{product.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium odio necessitatibus incidunt ipsum deserunt pariatur mollitia illum qui reprehenderit. Excepturi delectus odit porro necessitatibus rem eligendi minima modi eos, doloremque, soluta temporibus aliquam animi similique dicta neque nostrum recusandae asperiores molestias in illum non totam distinctio? Ut laborum magnam quidem accusamus neque. Officia doloremque saepe eius nostrum, officiis repellendus, dolores id esse accusantium qui soluta, iure itaque fugiat deleniti. Ex dolorum rerum inventore, et harum expedita quas nostrum laboriosam neque aut officiis assumenda iure facere labore maxime deserunt officia asperiores nesciunt id a fuga sed eum hic animi! Reiciendis? </div>
+                        <div className='font-semibold text-4xl'><sup>Rp</sup> {numberFormat(product.price)}</div>
 
-                        <Button>Add to cart</Button>
+                        <Button className='mt-20'>Add to cart</Button>
                     </div>
                 </div>
             </Container>
