9fb7c0b75459edaf1d0e2156a3382a22a566aa1b
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-05-20 11:44:35 +0700

    99.8%

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,10 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ScanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('name');
-
-Route::resource('products', ProductController::class);
-
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
+    Route::resource('products', ProductController::class);
+    Route::get('/', [HomeController::class, 'index'])->name('name');
 
     Route::get('scan', [ScanController::class, 'index'])->name('scan.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
